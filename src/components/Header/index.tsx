0e3673af8f5d29fa82113ac2b7ed0cebbce7b0dd
lqr131115 <1046975211@qq.com>
2024-05-07 21:33:50 +0800

    feat: avatar after login

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Button, Space } from "antd";
+import { Button, Space, Popover } from "antd";
 import classNames from "classnames";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Image from "next/image";
 import type { NextPage } from "next";
+import { useStore } from "@/store";
 import { nav } from "./config";
 import Login from "../Login";
 import styles from "./header.module.scss";
@@ -13,6 +15,7 @@ const Header: NextPage = () => {
   const { pathname } = router;
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [deadline, setDeadline] = useState(0);
+  const store = useStore();
   const goToLogin = () => {
     setIsModalOpen(true);
   };
@@ -43,9 +46,25 @@ const Header: NextPage = () => {
         <section className={styles.action}>
           <Space>
             <Button>写文章</Button>
-            <Button type="primary" onClick={goToLogin}>
-              登录
-            </Button>
+            {store.user.userInfo.id ? (
+              <div className={styles.avatar}>
+                <Popover
+                  content={<Button type="link">退出登录</Button>}
+                  trigger="click"
+                >
+                  <Image
+                    width={40}
+                    height={40}
+                    alt="avatar"
+                    src={store.user.userInfo.avatar}
+                  />
+                </Popover>
+              </div>
+            ) : (
+              <Button type="primary" onClick={goToLogin}>
+                登录
+              </Button>
+            )}
           </Space>
         </section>
       </header>
