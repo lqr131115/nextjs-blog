817b78655e18c1f7c14c57f79dbf0902130f0b6a
lqr131115 <1046975211@qq.com>
2024-05-09 16:36:22 +0800

    styles: article list

--- src/components/List/article.tsx
+++ src/components/List/article.tsx
@@ -1,29 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { IArticle } from "@/pages/api";
-import { Avatar, List, Space } from "antd";
-import Image from "next/image";
-import type { NextPage } from "next";
+import { List, Space } from "antd";
 import { StarOutlined, LikeOutlined, MessageOutlined } from "@ant-design/icons";
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "./article.module.scss";
+import { useRouter } from "next/router";
 interface IProps {
   articles: IArticle[];
 }
 
 const ArticleList: NextPage<IProps> = ({ articles }) => {
-  console.log("articles", articles);
-
+  const { push } = useRouter();
   const IconText = ({ icon, text }: { icon: React.FC; text: string }) => (
     <Space>
       {React.createElement(icon)}
       {text}
     </Space>
   );
+
   return (
     <>
       <List
-        itemLayout="horizontal"
+        itemLayout="vertical"
         dataSource={articles}
         renderItem={(item) => (
           <List.Item
+            onClick={() => push(`/article/${item.id}`)}
+            style={{ padding: 10 }}
+            className={styles.item}
             key={item.title}
             actions={[
               <IconText
@@ -44,16 +49,22 @@ const ArticleList: NextPage<IProps> = ({ articles }) => {
             ]}
             extra={
               item.cover ? (
-                <Image width={272} height={272} alt="cover" src={item.cover} />
+                <Image width={162} height={100} alt="cover" src={item.cover} />
               ) : null
             }
           >
             <List.Item.Meta
-              avatar={<Avatar src={item.user.avatar} />}
-              title={<a href={`/article/${item.id}`}>{item.title}</a>}
+              avatar={
+                <Image
+                  width={30}
+                  height={30}
+                  alt="avatar"
+                  src={item.user.avatar}
+                />
+              }
+              title={item.title}
               description={item.description}
             />
-            {item.content}
           </List.Item>
         )}
       />
