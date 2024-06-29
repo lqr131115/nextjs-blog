4fbc6598725eed2e41e34ba91805da24b78bb556
lqr131115 <1046975211@qq.com>
2024-06-29 16:50:42 +0800

    chore: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div>Hello, Next.js!</div>;
+import type { NextPage } from "next";
+import { Article } from "@/db/entity";
+import { getRepository } from "@/db";
+import ArticleList from "@/components/List/article";
+import styles from "./index.module.scss";
+
+export async function getServerSideProps() {
+  const articleRep = await getRepository(Article);
+  // 也可直接使用store中的UserInfo, 此处使用级联
+  const articles = await articleRep.find({ relations: ["user"] });
+
+  return {
+    props: {
+      articles: JSON.parse(JSON.stringify(articles)),
+    },
+  };
 }
+
+const Home: NextPage = (props: any) => {
+  const { articles } = props;
+  return (
+    <div className={styles.wrapper}>
+      <ArticleList articles={articles} />
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,28 +1,7 @@
-import type { NextPage } from "next";
-import { Article } from "@/db/entity";
-import { getRepository } from "@/db";
-import ArticleList from "@/components/List/article";
-import styles from "./index.module.scss";
+import { useRouter } from "next/router";
 
-export async function getServerSideProps() {
-  const articleRep = await getRepository(Article);
-  // 也可直接使用store中的UserInfo, 此处使用级联
-  const articles = await articleRep.find({ relations: ["user"] });
-
-  return {
-    props: {
-      articles: JSON.parse(JSON.stringify(articles)),
-    },
-  };
+export default function UserPage() {
+  const { query } = useRouter();
+  const { id } = query;
+  return <div>Hello, User: {id}!</div>;
 }
-
-const Home: NextPage = (props: any) => {
-  const { articles } = props;
-  return (
-    <div className={styles.wrapper}>
-      <ArticleList articles={articles} />
-    </div>
-  );
-};
-
-export default Home;
